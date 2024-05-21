869edd3b1ed3a26f0dde7eefa747ac105c1e1a7e
xiaojunzaizai <xiaojunzaizai@gmail.com>
2024-05-21 01:44:22 -0400

    完善展示任务所需的远征的一部分功能

--- src/app/expedition-info/expedition-info.component.ts
+++ src/app/expedition-info/expedition-info.component.ts
@@ -9,7 +9,7 @@ import { ExpeditionInfo, ExpeditionQuest,ExpeditionInfoInQuest } from '../interf
   templateUrl: './expedition-info.component.html',
   styleUrl: './expedition-info.component.css'
 })
-export class ExpeditionInfoComponent implements OnInit {
+export class ExpeditionInfoComponent implements OnInit, OnChanges {
   @Input() expedition: ExpeditionInfo[]  = [];
   @Input() selectedQuests: ExpeditionQuest[] = [];
   selectedQuestsExpeditionList:ExpeditionInfoInQuest[]=[];
@@ -17,11 +17,40 @@ export class ExpeditionInfoComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+      if(changes['selectedQuests']) {
+        this.reset_expedition_list();
+        this.get_expedition_list();
+      }
+  }
+
   get_expedition_list(){
     this.selectedQuests.forEach(quest =>{
       quest.expeditions.forEach(item =>{
-        this.selectedQuestsExpeditionList.push(item);
+        const index = this.selectedQuestsExpeditionList.findIndex(exp => exp.expedition_code === item.expedition_code);
+        if (index !== -1) {
+          // Update the existing item
+          // const updatedItem = {
+          //   ...this.selectedQuestsExpeditionList[index],
+          //   expedition_need_count: this.selectedQuestsExpeditionList[index].expedition_need_count + item.expedition_need_count
+          // };
+          // this.selectedQuestsExpeditionList[index] = updatedItem; 
+          // Update the existing item
+          const existingItem = this.selectedQuestsExpeditionList[index];
+          existingItem.expedition_need_count += item.expedition_need_count;
+          
+          // Check if the quest code is already in the list
+          if (!existingItem.quest_codes.includes(quest.code)) {
+              existingItem.quest_codes.push(quest.code);
+          }       
+        } else {
+          // Add the new item with the additional field as a list
+          this.selectedQuestsExpeditionList.push({ ...item, quest_codes: [quest.code] });        }
       })
     })
+  }
+
+  reset_expedition_list(){
+    this.selectedQuestsExpeditionList = [];
   }
 }
--- src/app/interface/expedition-info-in-quest.ts
+++ src/app/interface/expedition-info-in-quest.ts
@@ -1,8 +1,9 @@
 import { ExpeditionStatus } from "./expedition-status";
 export interface ExpeditionInfoInQuest {
+    quest_codes: any;
     name: string;
     expedition_code: string;
     expedition_consume_time: string|number;
-    expedition_need_count: number|string;
+    expedition_need_count: number;
     condition_status: ExpeditionStatus;
 }
